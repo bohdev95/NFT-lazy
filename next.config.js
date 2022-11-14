6af26632992cc9574679eddc38b9e4f2b8e17028
bohdev95 <boleinikov95@gmail.com>
2022-11-14 02:00:27 -0500

    integrate webgl

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   // sassOptions: {
   //   includePaths: [path.join(__dirname, "styles")],
   // },
+  webpack: (config, { dev }) => {
+    config.module.rules.push(
+      {
+          test: /\.(glsl|frag|vert)$/,
+          exclude: /node_modules/,
+          use: ['glslify-import-loader', 'raw-loader', 'glslify-loader']
+      },
+    )
+    return config
+  }
 };
+
 
 module.exports = nextConfig;
